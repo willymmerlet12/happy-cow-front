4b18da70b4a33003c89d13593b8487c7140a1163
Willy <willymmerlet@gmail.com>
2020-12-09 15:46:01 +0100

    heroku added

--- src/containers/Signup/Signup.js
+++ src/containers/Signup/Signup.js
@@ -56,7 +56,7 @@ const Signup = ({ setUser }) => {
       event.preventDefault();
       if (username && email && password) {
         const response = await axios.post(
-          "https://happy-cow-replica.herokuapp.com//signup",
+          "https://happy-cow-replica.herokuapp.com/signup",
           formData
         );
         console.log(response.data);
