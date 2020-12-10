8dba039f67accbee32aa25f0975fc16b090fd43b
Willy <willymmerlet@gmail.com>
2020-12-10 11:20:06 +0100

    finito

--- src/containers/Feed/FeedTheCow.js
+++ src/containers/Feed/FeedTheCow.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import Checkout from "../../components/Checkout/CheckOut";
